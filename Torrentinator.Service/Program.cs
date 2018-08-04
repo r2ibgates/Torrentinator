d14ef1991a1f861ae86a86aa5895f9b1410fa556
Bob Gates <bgates@r2integrated.com>
2018-08-04 08:50:13 -0400

    Update to 2.1.

---
+++ Torrentinator.Service/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Microsoft.Extensions.Hosting;
+
+namespace Torrentinator.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+            var builder = CreateWebHostBuilder(args.Where(arg => arg != "--console").ToArray());
+
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                builder.UseContentRoot(pathToContentRoot);
+            }
+
+            var host = builder.Build();
+
+            if (isService)
+            {
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => new HostBuilder()
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddJsonFile("appsettings.json", optional: true);
+                config.AddEnvironmentVariables();
+
+                if (args != null)
+                {
+                    config.AddCommandLine(args);
+                }
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddOptions();
+                services.Configure<AppConfig>(hostContext.Configuration.GetSection("AppConfig"));
+
+                services.AddSingleton<IHostedService, PrintTextToConsoleService>();
+            })
+            .ConfigureLogging((hostingContext, logging) => {
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+            });
+    }
+}
